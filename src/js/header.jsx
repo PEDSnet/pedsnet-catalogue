e50fd51f3695009eddc23661c14dc5a4a167bb3a
l-k- <alenka0000@gmail.com>
2015-12-09 21:43:08 -0500

    Add application base path to header URL

--- src/js/header.jsx
+++ src/js/header.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
 
+var root = require('./env').root;
 
 var Header = React.createClass({
     render: function() {            
         return (
             <header>
                 <div className='header-row'>
-                    <a className='brand' tabIndex={-1} href='/'><img src='/img/brand.png' /> PEDSnet Catalogue</a>
+                    <a className='brand' tabIndex={-1} href={root}><img src={root+'/img/brand.png'} /> PEDSnet Catalogue</a>
                 </div>
             </header>
         );
